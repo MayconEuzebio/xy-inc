3ddef4428e5c0a286c985af2bf71ee46ca21060f
Maycon <euzebio.maycon@gmail.com>
2016-07-20 03:38:05 -0300

    Module mongoose-integer e teste1

--- public/index/IndexController.js
+++ public/index/IndexController.js
@@ -20,6 +20,51 @@ angular.module("app").controller("IndexController",
                 });
         };
         
+        $scope.testeServ1 = function () {
+            POI.query(
+                function(list) {
+                    if(list.length > 0){
+                        return;
+                    }
+
+                    var objs = [
+                        {'name': 'Lanchonete',
+                            'x': '27',
+                            'y': '12'
+                        },
+                        {'name': 'Posto',
+                            'x': '31',
+                            'y': '18'
+                        },
+                        {'name': 'Joalheria',
+                            'x': '15',
+                            'y': '12'
+                        },
+                        {'name': 'Floricultura',
+                            'x': '19',
+                            'y': '21'
+                        },
+                        {'name': 'Pub',
+                            'x': '12',
+                            'y': '8'
+                        },
+                        {'name': 'Supermercado',
+                            'x': '23',
+                            'y': '6'
+                        },
+                        {'name': 'Churrascaria',
+                            'x': '28',
+                            'y': '2'
+                        }];
+
+                    for(var key in objs){
+                        $scope.poi = new POI(objs[key]);
+                        $scope.savePOI();
+                    }       
+
+                });
+        };
+        
         $scope.displayPOIs = function () {
 
             POI.query(
